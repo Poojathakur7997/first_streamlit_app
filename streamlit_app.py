f0d6214a434a507e8d3d173a4f0c2041564b2169
Poojathakur7997 <120466783+Poojathakur7997@users.noreply.github.com>
2022-12-15 18:11:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,13 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast ')
 
 # New Section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 
 
 
