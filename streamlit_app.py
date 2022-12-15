17c00e5c416a7bcfa5c716835991bc550bce3173
Poojathakur7997 <120466783+Poojathakur7997@users.noreply.github.com>
2022-12-15 23:49:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast ')
 
-import snowflake-connector
-
-
-
-
-
-
-
-
-
+import snowflake.connector
